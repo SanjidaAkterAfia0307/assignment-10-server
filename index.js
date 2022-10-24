bc489a69818486fe50f82c275516098983aae384
Afia <sanjidaakterafia0307@gmail,com>
2022-10-24 23:24:49 +0600

    first

---
+++ index.js
@@ -0,0 +1,19 @@
+const express = require('express')
+const cors=require("cors")
+const app = express()
+const port = process.env.PORT || 5000
+
+app.use(cors())
+
+const courses=require("./data/courses.json")
+
+app.get('/', (req, res) => {
+  res.send('Hello World!')
+})
+app.get('/courses', (req, res) => {
+  res.send(courses)
+})
+
+app.listen(port, () => {
+  console.log(`This app listening on port ${port}`)
+})
