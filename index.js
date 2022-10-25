2aaebe0166218e0bb666d0744ceb787e74009f08
Afia <sanjidaakterafia0307@gmail,com>
2022-10-25 15:55:45 +0600

    details and route added

--- index.js
+++ index.js
@@ -1,17 +1,24 @@
 const express = require('express')
-const cors=require(cors)
+const cors = require("cors")
 const app = express()
 const port = process.env.PORT || 5000
 
 app.use(cors())
 
-const courses=require("./data/courses.json")
+const courses = require("./data/courses.json")
 
 app.get('/', (req, res) => {
   res.send('Hello World!')
 })
 app.get('/courses', (req, res) => {
   res.send(courses)
+})
+app.get('/courses/:id', (req, res) => {
+  const id = parseInt(req.params.id);
+  console.log(id)
+  const selectedCourse = courses.find(course => course.id === id)
+  console.log(selectedCourse)
+  res.send(selectedCourse)
 })
 
 app.listen(port, () => {
